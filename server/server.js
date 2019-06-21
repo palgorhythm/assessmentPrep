f28be28a64485c615988f8c4d2a8c2607eea9ff7
Jacob Richards <jacob.richards33@gmail.com>
2019-06-21 12:51:47 -0700

    set up routes

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const controller = require('./sqlController');
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../', 'client'))); // serves all static files located in the client folder
+
+app.post('/signup', controller.signup, (req, res) => {
+  res.status(200).send('u signed up!');
+});
+app.post('/login', controller.login, (req, res) => {
+  res.status(200).send('u signed up!');
+});
+app.get('/getAllTodos/:uname', controller.getAllTodos, (req, res) => {
+  res.status(200).send('u signed up!');
+});
+app.post('/newTodo', controller.newTodo, (req, res) => {
+  res.status(200).send('u signed up!');
+});
+app.delete('/deleteTodo', controller.signup, (req, res) => {
+  res.status(200).send('u signed up!');
+});
+
+app.listen(3000, () => {
+  console.log('listening on port 3000 yeet');
+});
---
+++ server/sqlController.js
@@ -0,0 +1,19 @@
+const controller = {};
+
+controller.signup = (req, res) => {
+  next();
+};
+controller.login = (req, res) => {
+  next();
+};
+controller.getAllTodos = (req, res) => {
+  next();
+};
+controller.newTodo = (req, res) => {
+  next();
+};
+controller.deleteTodo = (req, res) => {
+  next();
+};
+
+module.exports = controller;
